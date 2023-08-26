0ea91bc7f989817881e9bdc7f0b747040efc0432
VipulaNeranjana <vipulana17@gmail.com>
2023-08-26 18:53:07 +0530

    create UserDTO

---
+++ src/main/java/lk/vipula/test/assignment/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package lk.vipula.test.assignment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDTO {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
